7a69957cf869a8104766cd7e50dc1d0a6ed7b14d
Donikannerr <67806377+Donikannerr@users.noreply.github.com>
2021-05-06 22:09:04 +0200

    add body_name

--- lib/screens/business_card.dart
+++ lib/screens/business_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_visitenkarte/main.dart';
 
 class BusinessCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(45),
         child: AppBar(
@@ -13,7 +15,37 @@ class BusinessCard extends StatelessWidget {
           title: Text("Visitenkarte"),
         ),
       ),
-      body: Text("Vorname: "),
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          height: 250,
+          width: 350,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.account_circle,
+                    size: 100,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Can Emre Genc",
+                        textScaleFactor: 2,
+                      ),
+                      Text("Flutter Entwickler"),
+                    ],
+                  )
+                ],
+              ),
+              Row(),
+              Row(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
