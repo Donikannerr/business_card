73048c1e9217c3e807a46926593b2be9ebf65ae4
Donikannerr <67806377+Donikannerr@users.noreply.github.com>
2021-05-06 19:05:17 +0200

    add title

--- lib/screens/business_card.dart
+++ lib/screens/business_card.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BusinessCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(45),
+        child: AppBar(
+          centerTitle: true,
+          backgroundColor: Color.fromRGBO(204, 153, 255, 100),
+          title: Text("Visitenkarte"),
+        ),
+      ),
+      body: Text("Vorname: "),
+    );
   }
 }
